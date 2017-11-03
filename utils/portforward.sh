672edf7f59ab35c0db30e7cb90edb3823f423069
Dmitry Shulyak <yashulyak@gmail.com>
2017-11-03 15:32:56 +0200

    Add port forward and run e2e

---
+++ utils/portforward.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# Portforward hack for CircleCI remote docker
+# copied from https://github.com/Mirantis/virtlet/blob/master/build/portforward.sh
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o errtrace
+
+if [[ ${1:-} = start ]]; then
+  docker run -d -it \
+         --name portforward --net=host \
+         --entrypoint /bin/sh \
+         bobrik/socat -c "while true; do sleep 1000; done"
+elif [[ ${1} ]]; then
+  socat "TCP-LISTEN:${1},reuseaddr,fork" \
+        EXEC:"'docker exec -i portforward socat STDIO TCP-CONNECT:localhost:${1}'"
+else
+  echo "Must specify either start or the port number" >&2
+  exit 1
+fi
